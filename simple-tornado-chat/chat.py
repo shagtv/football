967414a6e5fe5ed807f6c0290c768bb236594751
shagtv <shagtv@gmail.com>
2016-12-11 01:57:37 +0300

    fix

--- simple-tornado-chat/chat.py
+++ simple-tornado-chat/chat.py
@@ -13,6 +13,17 @@ from tornado.options import define, options, parse_command_line
 
 define('port', default=8000, help='run on the given port', type=int)
 
+class Bot:
+    def __init__(self, team, name):
+        self.x = random.randrange(30, 700)
+        self.y = random.randrange(30, 500)
+        self.move_x = 0
+        self.move_y = 0
+        self.team = team
+        self.active = True
+        self.speed = 3
+        self.name = name
+
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
@@ -32,10 +43,20 @@ class GameHandler(tornado.web.RequestHandler):
 
 class GameWebSocketHandler(tornado.websocket.WebSocketHandler, tornado.web.RequestHandler):
     connections = set()
+    started = False
 
     def open(self):
+        teams = ('home', 'guest')
+
         self.x = random.randrange(30, 700)
         self.y = random.randrange(30, 500)
+        self.speed = 3
+        self.team = teams[random.randrange(0, 2)]
+        self.move_x = 0
+        self.move_y = 0
+        self.active = False
+        self.name = 'noname'
+
         GameWebSocketHandler.connections.add(self)
 
     def on_close(self):
@@ -44,26 +65,75 @@ class GameWebSocketHandler(tornado.websocket.WebSocketHandler, tornado.web.Reque
     def on_message(self, msg):
         data = json.loads(msg)
 
+        if data['command'] == 'move':
+            self.move_x = data['moveX']
+            self.move_y = data['moveY']
+            GameWebSocketHandler.connections.add(self)
+        elif data['command'] == 'join':
+            if not self.active:
+                for p in list(GameWebSocketHandler.connections):
+                    if p.active and isinstance(p, Bot):
+                        self.team = p.team
+                        GameWebSocketHandler.connections.remove(p)
+                        self.active = True
+                        GameWebSocketHandler.connections.add(self)
+                        break
+        elif data['command'] == 'leave':
+            if self.active:
+                self.active = False
+                GameWebSocketHandler.connections.add(self)
+                player = Bot(self.team, 'Bot' + str(random.randrange(100)))
+                GameWebSocketHandler.connections.add(player)
+
+        elif data['command'] == 'start':
+            if not GameWebSocketHandler.started:
+                GameWebSocketHandler.started = True
+                for i in range(10):
+                    player = Bot('home', 'Bot' + str(i))
+                    GameWebSocketHandler.connections.add(player)
+                for i in range(10):
+                    player = Bot('guest', 'Bot' + str(i + 10))
+                    GameWebSocketHandler.connections.add(player)
+
+        elif data['command'] == 'stop':
+            if GameWebSocketHandler.started:
+                GameWebSocketHandler.started = False
+                for p in list(GameWebSocketHandler.connections):
+                    if isinstance(p, Bot):
+                        GameWebSocketHandler.connections.remove(p)
+                    else:
+                        p.active = False
+                        GameWebSocketHandler.connections.add(self)
+
+        elif data['command'] == 'save-name':
+            self.name = data['name']
+            GameWebSocketHandler.connections.add(self)
+
+    @staticmethod
+    def period_run():
         players = []
-        for i in GameWebSocketHandler.connections:
-            if  i == self:
-                i.x += data['moveX']
-                i.y += data['moveY']
-            players.append([i.x, i.y])
+        if GameWebSocketHandler.started:
+            for p in GameWebSocketHandler.connections:
+                if p.active:
+                    p.x += p.move_x
+                    p.y += p.move_y
+                    players.append([p.x, p.y, p.speed, p.team, p.name])
+
+        msg = {'command': 'draw', 'players': players}
 
         for i in GameWebSocketHandler.connections:
-            i.write_message({'command': 'check', 'players': players})
-
-    def send_messages(self, msg):
-        for conn in self.connections:
-            #conn.write_message({'name': self.current_user, 'msg': msg})
-            conn.write_message({'name': 'shagtv', 'msg': msg})
+            if isinstance(i, GameWebSocketHandler):
+                i.write_message(msg)
 
 def main():
     parse_command_line()
     app = Application()
     app.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+
+    loop = tornado.ioloop.IOLoop.instance()
+    period_cbk = tornado.ioloop.PeriodicCallback(GameWebSocketHandler.period_run, 20, loop)
+    period_cbk.start()
+    loop.start()
 
 
 if __name__ == '__main__':
